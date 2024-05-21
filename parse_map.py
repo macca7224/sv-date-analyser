0892ab0d178be610264260481f2dd683cefe2c26
macca7224 <138291010+macca7224@users.noreply.github.com>
2024-05-21 11:08:50 +1000

    Switch to using asyncio/aiohttp

--- parse_map.py
+++ parse_map.py
@@ -1,8 +1,8 @@
 import argparse
+import asyncio
 import csv
 import json
 import os
-import multiprocessing
 
 from tqdm import tqdm
 
@@ -11,30 +11,43 @@ from get_date import find_accurate_timestamp
 # Radius for date check
 RADIUS = 30
 
+# Increasing this will make the script faster, but you may get rate limited or 
+# have requests fail
+CHUNK_SIZE = 15
 
-def parse_loc(loc):
+
+async def parse_loc(loc, progress):
     lat, lng = loc['lat'], loc['lng']
     month = loc['imageDate']
 
     try:
-        timestamp = find_accurate_timestamp(lat, lng, month, RADIUS)
+        timestamp = await find_accurate_timestamp(lat, lng, month, RADIUS)
     except Exception:
         print(f'Failed to get date for {lat}, {lng}')
+        progress.update(1)
         return None
+    progress.update(1)
     return timestamp, lat, lng
 
 
-def bulk_parse(locs):
+async def bulk_parse(locs):
     total = len(locs)
-    with multiprocessing.Pool() as pool:
-        results = list(tqdm(pool.imap(parse_loc, locs), total=total))
+    chunks = [locs[i:i + CHUNK_SIZE] for i in range(0, total, CHUNK_SIZE)]
+    
+    results = []
+    progress = tqdm(total=total)
+    
+    for chunk in chunks:
+        tasks = [asyncio.create_task(parse_loc(loc, progress)) for loc in chunk]
+        chunk_results = await asyncio.gather(*tasks)
+        results.extend([res for res in chunk_results if res is not None])
 
-    results = [result for result in results if result is not None]
+    progress.close()
     return results
 
 
-def main(locs):
-    results = bulk_parse(locs)
+async def main(locs):
+    results = await bulk_parse(locs)
 
     name = os.path.basename(args.json_file).split('.')[0]
     output_path = f'csv/{name}.csv'
@@ -56,4 +69,4 @@ if __name__ == '__main__':
         json_data = json.load(f)
         locs = json_data['customCoordinates']
 
-    main(locs)
+    asyncio.run(main(locs))
